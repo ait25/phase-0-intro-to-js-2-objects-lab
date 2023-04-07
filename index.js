1571338576725aa2e0f6b1ba24d55e9b9b90fbe3
Alejandro Fernandez <fernandezale007@gmail.com>
2023-04-07 08:49:22 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution in this file!
+const employee = {
+    name: "John Doe",
+    streetAddress: "123 Main St"
+};
+
+const updateEmployeeWithKeyAndValue = (employeeObj, key, value) => {
+    return {
+        ...employeeObj,
+        [key]: value
+    };
+};
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employeeObj, key, value) => {
+    employeeObj[key] = value;
+    return employeeObj;
+};
+
+
+const deleteFromEmployeeByKey = (employeeObj, key) => {
+    const newObj = { ...employeeObj };
+    delete newObj[key];
+    return newObj;
+};
+
+const destructivelyDeleteFromEmployeeByKey = (employeeObj, key) => {
+    delete employeeObj[key];
+    return employeeObj;
+};
+
+console.log("Original employee:", employee);
+const updatedEmployee = updateEmployeeWithKeyAndValue(employee, "name", "Jane Smith");
+console.log("Updated employee (non-destructive):", updatedEmployee);
+console.log("Original employee (not mutated):", employee);
+destructivelyUpdateEmployeeWithKeyAndValue(employee, "streetAddress", "456 Oak Ave");
+console.log("Updated employee (destructive):", employee);
+const deletedEmployee = deleteFromEmployeeByKey(employee, "name");
+console.log("Employee with key deleted (non-destructive):", deletedEmployee);
+console.log("Original employee (not mutated):", employee);
+destructivelyDeleteFromEmployeeByKey(employee, "streetAddress");
+console.log("Employee with key deleted (destructive):", employee);
